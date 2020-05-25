2203e23ac70c071f37642b6ea87ad0c00729db24
danyweis <danyweismail@gmail.com>
2020-05-25 17:05:38 +0100

    created available colors object and have the function witch creats array with random numbers

--- guessColor/guessColor.js
+++ guessColor/guessColor.js
@@ -5,6 +5,15 @@ let helpBox = document.querySelector("#helpBox");
 let questionBTN = document.querySelector("#row2button5");
 let endBTN = document.querySelector("#row1button5");
 
+const availableColors = {
+    0: "rgb(33, 33, 209)", // blue
+    1: "rgb(33, 136, 33)", // green
+    2: "rgb(180, 30, 30)", // red
+    3: "rgb(238, 238, 33)", // yellow
+    4: "rgb(94, 33, 155)", // violet
+    5: "rgb(243, 89, 33)", // orange
+};
+
 //WELCOME PAGE
 
 // get out of the welcome page
@@ -20,7 +29,8 @@ function setDifficulty() {
         if (difficultyRadio[i].checked) {
             difficulty = difficultyRadio[i].value;
             welcomePage.classList.toggle("displayNone");
-            console.log(difficulty);
+            getRandomColor();
+
             break;
         }
     }
@@ -29,7 +39,6 @@ function setDifficulty() {
 function openWelcomePage() {
     welcomePage.classList.toggle("displayNone");
     difficulty = null;
-    console.log(difficulty);
 }
 
 //RULES PAGE
@@ -39,6 +48,31 @@ function openWelcomePage() {
 function openCloseRules() {
     helpBox.classList.toggle("displayNone");
 }
+
+// get the random colors
+
+function GameColors(randomColors) {}
+
+// object 0-5
+// colorBullet1 => blue
+// colorBullet2 => green
+// colorBullet3 => red
+// colorBullet4 => yellow
+// colorBullet5 => violet
+// colorBullet6 => orange
+
+function getRandomColor() {
+    let randomColorArray = [];
+    for (let i = 0; i < 4; i++) {
+        let getRandomNumber = Math.floor(Math.random() * 6);
+        randomColorArray.push(getRandomNumber);
+    }
+    return randomColorArray;
+}
+
+// Calling functions
+
+//get times from local storage
 
 //Event Listeners
 
