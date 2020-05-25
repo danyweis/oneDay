441ab07f3f8a42f16feba37bbda2b34cdc149187
danyweis <danyweismail@gmail.com>
2020-05-25 20:23:46 +0100

    get the user input now and only when the game is on

--- guessColor/guessColor.js
+++ guessColor/guessColor.js
@@ -4,15 +4,30 @@ let xHelpBox = document.querySelector("#xHelpBox");
 let helpBox = document.querySelector("#helpBox");
 let questionBTN = document.querySelector("#row2button5");
 let endBTN = document.querySelector("#row1button5");
+let color1BTN = document.querySelector("#row1button1");
+let color2BTN = document.querySelector("#row1button2");
+let color3BTN = document.querySelector("#row1button3");
+let color4BTN = document.querySelector("#row2button1");
+let color5BTN = document.querySelector("#row2button2");
+let color6BTN = document.querySelector("#row2button3");
+let validBTN = document.querySelector("#row1button4");
+let backBTN = document.querySelector("#row2button4");
 
-const availableColors = {
-    0: "rgb(33, 33, 209)", // blue
-    1: "rgb(33, 136, 33)", // green
-    2: "rgb(180, 30, 30)", // red
-    3: "rgb(238, 238, 33)", // yellow
-    4: "rgb(94, 33, 155)", // violet
-    5: "rgb(243, 89, 33)", // orange
-};
+// colorBullet1 => blue
+// colorBullet2 => green
+// colorBullet3 => red
+// colorBullet4 => yellow
+// colorBullet5 => violet
+// colorBullet6 => orange
+
+// store all the changes in the HTML to reset
+// everything at the end of the game
+let changesArry = [];
+// if gameOn is false we don't react on the input of the user
+let gameOn = false;
+
+// OBJECT TO STORE USER INPUT
+const userInputStorage = {};
 
 //WELCOME PAGE
 
@@ -29,6 +44,7 @@ function setDifficulty() {
         if (difficultyRadio[i].checked) {
             difficulty = difficultyRadio[i].value;
             welcomePage.classList.toggle("displayNone");
+            //Start the gme in asking rof first random colors
             getRandomColor();
 
             break;
@@ -49,11 +65,8 @@ function openCloseRules() {
     helpBox.classList.toggle("displayNone");
 }
 
-// get the random colors
+// GET THE RANDOM COLORS
 
-function GameColors(randomColors) {}
-
-// object 0-5
 // colorBullet1 => blue
 // colorBullet2 => green
 // colorBullet3 => red
@@ -63,19 +76,70 @@ function GameColors(randomColors) {}
 
 function getRandomColor() {
     let randomColorArray = [];
-    for (let i = 0; i < 4; i++) {
-        let getRandomNumber = Math.floor(Math.random() * 6);
+    for (let count = 1; count <= 4; count++) {
+        let placeholderArray = [];
+        // get the random color
+        let getRandomNumber = Math.ceil(Math.random() * 6);
+        document
+            .querySelector(`#resultColor${count}`)
+            .classList.toggle(`colorBullet${getRandomNumber}`);
+
+        placeholderArray.push(`#resultColor${count}`);
+        placeholderArray.push(`colorBullet${getRandomNumber}`);
+
+        // put in the changesArray what was changed in the html
+        changesArry.push(placeholderArray);
+
+        // randomColorArray is used later to compare with the user input
         randomColorArray.push(getRandomNumber);
     }
-    return randomColorArray;
+    gameOn = true;
+    userInputStorage.randomColor = randomColorArray;
+}
+// round will be incremented when one row is full and validated
+let round = 1;
+const userPlaceholder = [];
+function getUserInput(pushedColor) {
+    console.log(pushedColor);
 }
 
-// Calling functions
+function color1Pushed() {
+    gameOn ? getUserInput(1) : "";
+}
+function color2Pushed() {
+    gameOn ? getUserInput(2) : "";
+}
+function color3Pushed() {
+    gameOn ? getUserInput(3) : "";
+}
+function color4Pushed() {
+    gameOn ? getUserInput(4) : "";
+}
+function color5Pushed() {
+    gameOn ? getUserInput(5) : "";
+}
+function color6Pushed() {
+    gameOn ? getUserInput(6) : "";
+}
+function backPushed() {
+    gameOn ? getUserInput("back") : "";
+}
+function validPushed() {
+    gameOn ? getUserInput("valid") : "";
+}
 
 //get times from local storage
 
 //Event Listeners
 
+color1BTN.addEventListener("click", color1Pushed);
+color2BTN.addEventListener("click", color2Pushed);
+color3BTN.addEventListener("click", color3Pushed);
+color4BTN.addEventListener("click", color4Pushed);
+color5BTN.addEventListener("click", color5Pushed);
+color6BTN.addEventListener("click", color6Pushed);
+backBTN.addEventListener("click", backPushed);
+validBTN.addEventListener("click", validPushed);
 endBTN.addEventListener("click", openWelcomePage);
 questionBTN.addEventListener("click", openCloseRules);
 xHelpBox.addEventListener("click", openCloseRules);
