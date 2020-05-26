25efca8ed5df735f35fe8324d2015d420a9b72d6
danyweis <danyweismail@gmail.com>
2020-05-26 12:57:49 +0100

    wee now know if win or game over

--- 007guessColor/007guessColor.js
+++ 007guessColor/007guessColor.js
@@ -13,28 +13,19 @@ let color6BTN = document.querySelector("#row2button3");
 let validBTN = document.querySelector("#row1button4");
 let backBTN = document.querySelector("#row2button4");
 
-// colorBullet1 => blue
-// colorBullet2 => green
-// colorBullet3 => red
-// colorBullet4 => yellow
-// colorBullet5 => violet
-// colorBullet6 => orange
-
-// store all the changes in the HTML to reset
-// everything at the end of the game
+// store all the changes in the HTML to reset everything at the end of the game
 let changesArray = [];
 
 // if gameOn is false we don't react on the input of the user
 let gameOn = false;
 
 // OBJECT TO STORE USER INPUT
-const userInputStorage = {};
+const userInputStorage = {}; // Look how to empty the
 
 //WELCOME PAGE
 
 // get out of the welcome page
-// ==> set difficulty to null to play the game <== AWAY
-let difficulty = 8;
+let difficulty = 8; // <== set difficulty to null to play the game <== (AWAY)
 let welcomePage = document.querySelector("#welcome");
 
 function setDifficulty() {
@@ -44,13 +35,7 @@ function setDifficulty() {
     // go over the array and get the selected difficulty
     for (let i = 0; i < difficultyRadio.length; i++) {
         if (difficultyRadio[i].checked) {
-            if (difficultyRadio[i].value === "easy") {
-                difficulty = 12;
-            } else if (difficultyRadio[i].value === "medium") {
-                difficulty = 10;
-            } else {
-                difficulty = 8;
-            }
+            difficulty = difficultyRadio[i].value;
             welcomePage.classList.toggle("displayNone");
             //Start the gme in asking rof first random colors
             getRandomColor();
@@ -61,37 +46,20 @@ function setDifficulty() {
 }
 
 function openWelcomePage() {
-    console.log(changesArray);
-
-    for (let n = 0; n < changesArray.length; n++) {
-        document.querySelector(`${changesArray[n][0]}`).classList.toggle(`${changesArray[n][1]}`);
-    }
-    // console.log(changesArray);
-
-    if (round < 5) {
-        document.querySelector(`#row${countRows}ChoiceColor${round}`).classList.toggle("waitForAction");
-    }
-
-    countRows = 1;
-    round = 1;
-
-    document.querySelector(`#row${countRows}ChoiceColor${round}`).classList.toggle("waitForAction");
-
-    changesArray = [];
-    // console.log(`empty ${changesArray}`);
-    welcomePage.classList.toggle("displayNone");
-    difficulty = null;
+    // reset all the styling
+    resetAllTheCSS();
+    // reset array, object and difficulty
+    resetAllTheData();
 }
 
 //RULES PAGE
 
 // open and close the rules / help box page
-
 function openCloseRules() {
     helpBox.classList.toggle("displayNone");
 }
 
-// GET THE RANDOM COLORS
+// START THE GAME GET THE RANDOM COLORS
 
 // colorBullet1 => blue
 // colorBullet2 => green
@@ -120,7 +88,8 @@ function getRandomColor() {
     }
     gameOn = true;
     userInputStorage.randomColor = randomColorArray;
-    console.log(randomColorArray);
+
+    // console.log(randomColorArray);
 }
 
 // count will be incremented when one row is full and validated
@@ -129,12 +98,12 @@ let countRows = 1;
 // round will count the user inputs
 let round = 1;
 
+let userInputNumbers = [];
 function getUserInput(pushedColor) {
     const userPlaceholder = [];
     if (countRows <= difficulty) {
         if (pushedColor >= 1 && pushedColor <= 6 && round <= 4) {
             // userPlaceholder.push(pushedColor);
-            // document.querySelector(`#row${countRows}ChoiceColor${round}`).classList.toggle(`colorBullet`);
             document.querySelector(`#row${countRows}ChoiceColor${round}`).classList.toggle(`colorBullet${pushedColor}`);
 
             // push the id and the classList to reset them later
@@ -150,32 +119,108 @@ function getUserInput(pushedColor) {
             if (round < 5) {
                 document.querySelector(`#row${countRows}ChoiceColor${round}`).classList.toggle("waitForAction");
             }
+
+            // push the pressed key to userInputNumbers
+            userInputNumbers.push(pushedColor);
         } else if (pushedColor === "back" && round !== 1 && round <= 5) {
             // pop out the last array from the
 
-            // document.querySelector(`${changesArray[changesArray.length - 1][0]}`).classList.toggle(`colorBullet`);
             document.querySelector(`${changesArray[changesArray.length - 1][0]}`).classList.toggle(`${changesArray[changesArray.length - 1][1]}`);
+
+            // while we not at the end of the row
             if (round !== 5) {
                 document.querySelector(`#row${countRows}ChoiceColor${round}`).classList.toggle("waitForAction");
             }
 
             document.querySelector(`${changesArray[changesArray.length - 1][0]}`).classList.toggle("waitForAction");
             round--;
+
+            // take last elements out of the arrays
             changesArray.pop();
+            userInputNumbers.pop();
         } else if (pushedColor === "valid" && round === 5) {
             // DO THE MAGIC LOOK IF THE COLORS ARE CORRECT
+            // get the data to the object
+            userInputStorage[countRows] = userInputNumbers;
+            userInputNumbers = [];
             round = 1;
-            countRows++;
-            document.querySelector(`#row${countRows}ChoiceColor${round}`).classList.toggle("waitForAction");
 
-            console.log("lets do this");
+            //This will look if we did win
+            checkIfWin();
+
+            // If we did not win we have to look if the colors are correct
+            checkCorrectColors();
+
+            countRows++;
+            if (countRows <= difficulty) {
+                document.querySelector(`#row${countRows}ChoiceColor${round}`).classList.toggle("waitForAction");
+            }
         }
     } else {
         // GAME OVER
+        console.log("Game over");
     }
     // to see what is pushed
-    console.log(pushedColor);
+    // console.log(pushedColor);
 }
+
+// just looks if WIN or NOT
+function checkIfWin() {
+    for (let n = 0; n < userInputStorage["randomColor"].length; n++) {
+        if (userInputStorage["randomColor"][n] !== userInputStorage[countRows][n]) {
+            console.log("No not this time");
+            return;
+        } else {
+            console.log("do something you did win");
+        }
+    }
+}
+
+function checkCorrectColors() {
+    let randomArray = userInputStorage["randomColor"].slice();
+    let userArray = userInputStorage[countRows].slice();
+    for (let n = randomArray.length - 1; n >= 0; n--) {}
+}
+
+// reset all the HTML CSS when go to welcome page or win or lose the game
+
+function resetAllTheCSS() {
+    for (let n = 0; n < changesArray.length; n++) {
+        document.querySelector(`${changesArray[n][0]}`).classList.toggle(`${changesArray[n][1]}`);
+    }
+    if (round < 5 && countRows < difficulty) {
+        document.querySelector(`#row${countRows}ChoiceColor${round}`).classList.toggle("waitForAction");
+    }
+    //     if (countRows ==  difficulty) {
+    //         document.querySelector(`#row${countRows}ChoiceColor${round}`).classList.toggle("waitForAction");
+
+    // }
+    // we reset the object before resetting countRows
+    resetTheObject(countRows);
+
+    countRows = 1;
+    round = 1;
+    // put the wait for action back to the first hole
+    document.querySelector(`#row${countRows}ChoiceColor${round}`).classList.toggle("waitForAction");
+    welcomePage.classList.toggle("displayNone");
+}
+
+// reset the object to become empty
+function resetTheObject(countRows) {
+    for (let n = 1; n <= countRows; n++) {
+        delete userInputStorage[n];
+    }
+    delete userInputStorage["randomColor"];
+}
+
+// reset all the data when go to welcome page or win or lose the game
+function resetAllTheData() {
+    userInputNumbers = [];
+    changesArray = [];
+    difficulty = null;
+}
+
+// Actions for the event listeners
 
 function color1Pushed() {
     gameOn ? getUserInput(1) : "";
