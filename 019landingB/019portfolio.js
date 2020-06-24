a3dbf3293883086bfbb9a180f7b3aa13454a25cb
danyweis <danyweismail@gmail.com>
2020-06-24 23:40:41 +0100

    made layout menu

---
+++ 019landingB/019portfolio.js
@@ -0,0 +1,10 @@
+// import portfolioData from "./019portfolioJSON.js";
+var app = new Vue({
+  el: "#app",
+  // components: {
+  //   portfolioData,
+  // },
+  data: {
+    portfolio: portfolioData,
+  },
+});
---
+++ 019landingB/019portfolioJSON.js
@@ -0,0 +1,96 @@
+let portfolioData = {
+  projects: [
+    {
+      type: "Landing",
+      image: "019portfolioIMG/",
+      projectName: "Momentum clone",
+      projectLink:
+        "https://danyweis.com/Project/smallProjects/001momentum.html",
+      codeLink:
+        "https://github.com/danyweis/oneDay/tree/master/001MomentumClone",
+    },
+    {
+      type: "Landing",
+      image: "019portfolioIMG/",
+      projectName: "Momentum clone",
+      projectLink:
+        "https://danyweis.com/Project/smallProjects/001momentum.html",
+      codeLink:
+        "https://github.com/danyweis/oneDay/tree/master/001MomentumClone",
+    },
+    {
+      type: "Landing",
+      image: "019portfolioIMG/",
+      projectName: "Momentum clone",
+      projectLink:
+        "https://danyweis.com/Project/smallProjects/001momentum.html",
+      codeLink:
+        "https://github.com/danyweis/oneDay/tree/master/001MomentumClone",
+    },
+    {
+      type: "Landing",
+      image: "019portfolioIMG/",
+      projectName: "Momentum clone",
+      projectLink:
+        "https://danyweis.com/Project/smallProjects/001momentum.html",
+      codeLink:
+        "https://github.com/danyweis/oneDay/tree/master/001MomentumClone",
+    },
+    {
+      type: "Landing",
+      image: "019portfolioIMG/",
+      projectName: "Momentum clone",
+      projectLink:
+        "https://danyweis.com/Project/smallProjects/001momentum.html",
+      codeLink:
+        "https://github.com/danyweis/oneDay/tree/master/001MomentumClone",
+    },
+  ],
+  blog: [
+    {
+      postName: "This is my Blogpost",
+      image: "019portfolioIMG/",
+      author: "Admin",
+      date: "June 24, 2020",
+      postText:
+        "lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem",
+      blogLink: "https://danyweis.com/Project/project.html",
+    },
+    {
+      postName: "This is my Blogpost",
+      image: "019portfolioIMG/",
+      author: "Admin",
+      date: "June 24, 2020",
+      postText:
+        "lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem",
+      blogLink: "https://danyweis.com/Project/project.html",
+    },
+    {
+      postName: "This is my Blogpost",
+      image: "019portfolioIMG/",
+      author: "Admin",
+      date: "June 24, 2020",
+      postText:
+        "lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem",
+      blogLink: "https://danyweis.com/Project/project.html",
+    },
+    {
+      postName: "This is my Blogpost",
+      image: "019portfolioIMG/",
+      author: "Admin",
+      date: "June 24, 2020",
+      postText:
+        "lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem",
+      blogLink: "https://danyweis.com/Project/project.html",
+    },
+    {
+      postName: "This is my Blogpost",
+      image: "019portfolioIMG/",
+      author: "Admin",
+      date: "June 24, 2020",
+      postText:
+        "lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem",
+      blogLink: "https://danyweis.com/Project/project.html",
+    },
+  ],
+};
